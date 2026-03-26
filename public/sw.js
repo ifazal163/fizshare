a64fb9c45355d0be3fcacf2728e07b69ad43e401
ifazal163 <asktofazal@gmail.com>
2026-03-26 21:32:39 +0500

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,7 @@
+const CACHE_NAME = 'fizshare-v2';
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['/'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
